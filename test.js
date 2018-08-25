a7d4abd757ab562597454bba308bc6c805d19792
xuantran <tranthanhxuan.chef@gmail.com>
2018-08-25 21:03:24 +0700

    commit js

--- test.js
+++ test.js
@@ -2,7 +2,10 @@
 // var user = ["a", 'b',`c`]
 // var age = 5
 // console.log(`age: ${age}`)
+
 // pure fuction
-function add(y) {
-    return x + y;
+function add(x, y) {
+    return x = x + y;
 }
+// arrow function
+add = (x, y) => x + y
